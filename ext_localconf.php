8111e42b5904a4f955e449834478eaef867806f4
Amadeus Kiener <a.kiener@unibrand.de>
2025-01-23 13:27:02 +0100

    fix viewhelpers

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,9 @@
 <?php
 defined('TYPO3') or die();
 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['shape'] = ['UBOS\Shape\ViewHelpers'];
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['tx_shape_input_field'] = 'EXT:shape/Configuration/RTE/InputField.yaml';
+
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][573765204] = 'EXT:shape/Resources/Private/Templates/Finisher/Mail/';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
